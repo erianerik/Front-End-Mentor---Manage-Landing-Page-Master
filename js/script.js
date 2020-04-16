f7488b4a4f957a2b5027770f6add302f0cd77bef
erianerik <erianerik@gmail.com>
2020-04-16 19:40:04 -0300

    Projeto finalizado

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$( "#menu-mobile" ).click(function() {
+    $("#menu-itens").css("display", "flex");
+});
+
+$( "#menu-mobile" ).click(function() {
+    $("#menu-itens li").css("display", "flex");
+});
+
+$( "#mobile-close" ).click(function() {
+    $("#menu-itens li").css("display", "none");
+});
+
+ 
